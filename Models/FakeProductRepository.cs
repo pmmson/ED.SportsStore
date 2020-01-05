a4a816cdea14ba72b8797660ec49faf4ba30d3cb
pmmson <pmmson@gmail.com>
2020-01-05 14:14:57 +0300

    создание модели Product создание фейкового хранилища и доступа к нему

---
+++ Models/FakeProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product { Name = "Football", Price = 25 },
+            new Product { Name = "Surf board", Price = 179 },
+            new Product { Name = "Running shoes", Price = 95 }
+
+        };
+
+        public IQueryable<Product> Products
+        {
+            get
+            {
+                return products.AsQueryable();
+            }
+        }
+    }
+}
