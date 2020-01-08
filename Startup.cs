68cd57cd0fae3c45812d0f3372be270e26b2426c
pmmson <pmmson@gmail.com>
2020-01-08 12:24:12 +0300

    создание постоянного хранилища данных БД ORACLE заполнение хранилища начальными данными

--- Startup.cs
+++ Startup.cs
@@ -8,16 +8,26 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseOracle(Configuration["Data:SportsStoreProducts:ConnectionString"]));
+            //services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddTransient<IProductRepository, EFProductRepository>();
+
             services.AddMvc();
         }
 
@@ -32,6 +42,8 @@ namespace SportsStore
             {
                 routes.MapRoute("default", "{controller=Product}/{action=List}/{id?}");
             });
+
+            SeedData.EnsurePopulated(app);
         }
     }
 }
